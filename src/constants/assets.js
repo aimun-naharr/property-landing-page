f8fddf534e5ae2fb4ff26322f3c1d5e0dd1ebaa0
aimun nahar <aimunnaharr@gmail.com>
2024-03-28 15:44:31 +0600

    first commit

---
+++ src/constants/assets.js
@@ -0,0 +1,9 @@
+import image1 from "../../public/assets/image1.avif";
+import image2 from "../../public/assets/image2.avif";
+import image3 from "../../public/assets/image3.avif";
+import image4 from "../../public/assets/image4.avif";
+import image5 from "../../public/assets/image5.avif";
+import image6 from "../../public/assets/image6.avif";
+
+
+export const heroSectionImages = { image1, image2, image3, image4, image5, image6 }
